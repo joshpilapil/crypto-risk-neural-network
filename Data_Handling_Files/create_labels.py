03261ffb7a7801417b87c55ef1b13531ea2acaea
joshpilapil <joshuapilapil2016@gmail.com>
2026-06-04 13:12:42 -0700

    FInished logistic regression model. Overall achieved 57% accuracy. Tried other methods to raise it but it seems this is the limit. Am moving to adjust model to move from predicting the risk classes and be more sure toward low risk for investments.

--- Data_Handling_Files/create_labels.py
+++ Data_Handling_Files/create_labels.py
@@ -29,18 +29,15 @@ for i in range(len(df)):
 
 # Add future drawdown to the dataframe
 df["future_drawdown_48h"] = future_drawdowns
-q1 = df["future_drawdown_48h"].quantile(0.33)
-q2 = df["future_drawdown_48h"].quantile(0.66)
-print(q1, q2)
 
 # Define risk labels based on future drawdown thresholds
 def assign_risk_label(drawdown):
-    if drawdown <= q1:
-        return "High"
-    elif drawdown <= q2:
+    if drawdown > -0.03:
+        return "Low"
+    elif drawdown > -0.08:
         return "Medium"
     else:
-        return "Low"
+        return "High"
 
 # Apply risk labeling based on future drawdown
 df["risk_label"] = df["future_drawdown_48h"].apply(assign_risk_label)
